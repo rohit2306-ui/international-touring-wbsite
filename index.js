e6e4435bdb0095bb2046b4d3b271ffb057202b48
Rohit thakur <140095379+rohit2306-ui@users.noreply.github.com>
2024-11-12 00:27:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let profileit = document.querySelector(".profile_photo")
+let pro = document.querySelector(".profile_about")
+
+profileit.onclick = function(){
+    pro.classList.toggle('active')
+}
+
+let imp = document.querySelector('.our-imp2')
+
+imp.addEventListener("mouseenter" , function(){
+    classList.toggle('active')
+})
+
+
